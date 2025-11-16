03e3514d1e9f7e85ef58a1647486462090d35a7c
Hiromu <lucian@shink-it.com>
2025-11-16 22:35:04 +0900

    fix : google2

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/document";
+import { NextResponse } from "next/server";
 import { createSupabaseServerClient } from "@/lib/supabase/server";
 
 export async function GET(request: Request) {
